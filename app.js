57cc5dbdbf734aca8f0a9f0d2b8400ca6c5bcd91
hellovaughansmith <hello@vaughansmith.com.au>
2024-04-18 14:56:11 +1000

    Update from lessons

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ import API from "./services/API.js";
 import { loadData } from "./services/Menu.js";
 import Router from "./services/Router.js";
 
+// Link my web components
+import { MenuPage } from "./components/MenuPage.js";
+import { DetailsPage } from "./components/DetailsPage.js";
+import { OrderPage } from "./components/OrderPage.js";
+import ProductItem from "./components/ProductItem.js";
+
 window.app = {};
 app.store = Store;
 app.router = Router;
@@ -10,4 +16,11 @@ app.router = Router;
 window.addEventListener("DOMContentLoaded", () => {
   loadData();
   app.router.init();
+});
+
+window.addEventListener("appcartchange", (event) => {
+  const badge = document.getElementById("badge");
+  const qty = app.store.cart.reduce((acc, item) => acc + item.quanitity, 0);
+  badge.textContent = qty;
+  badge.hidden = qty == 0;
 });
--- services/Router.js
+++ services/Router.js
@@ -7,6 +7,10 @@ const Router = {
         Router.go(url);
       });
     });
+    // Event handler for URL changes
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
+    });
     // Check initial URL
     Router.go(location.pathname);
   },
@@ -14,6 +18,31 @@ const Router = {
     console.log(`Going to ${route}`);
     if (addToHistory) {
       history.pushState({ route }, "", route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("menu-page");
+        break;
+      case "http://127.0.0.1:5501/":
+        pageElement = document.createElement("menu-page");
+        break;
+      case "http://127.0.0.1:5501/order":
+        pageElement = document.createElement("order-page");
+        break;
+      default:
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("details-page");
+          const paramID = route.substring(route.lastIndexOf("-") + 1);
+          pageElement.dataset.productId = paramID;
+        }
+    }
+    if (pageElement) {
+      const cache = document.querySelector("main");
+      cache.innerHTML = "";
+      cache.appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
     }
   },
 };
